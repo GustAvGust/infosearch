2b0aca2895b7f533e91d6b2e3ab4244050711154
MakK <Ooogrob@yandex.ru>
2023-02-22 19:06:39 +0300

    add website files

--- task1/main.py
+++ task1/main.py
@@ -20,15 +20,17 @@ for item in list_of_sites:
         r"https?://(www.)?[a-z\-1-9]*\.(ru|com|org|info)", str(tree.find('style'))
     )
     for site in result:
+        if count == 100:
+            break
         try:
             response_local = requests.get(site.group(0))
             if response_local.status_code == 200:
-                site_list.append(site.group(0))
                 count += 1
                 tree_local = bs4.BeautifulSoup(response_local.text, 'html.parser')
                 name = str(count) + ".html"
+                site_list.append(name + ": " + str(site.group(0)))
                 with open(name, "w", encoding = 'utf-8') as file:
                     file.write(str(tree_local))
         except: pass
 with open("index.txt", "w", encoding='utf-8') as file:
-    file.write("\n".join(map(str, site_list)))
+    file.write("\n".join(site_list))
